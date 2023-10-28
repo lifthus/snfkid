79125cd0b58768d9dd042d6127e1b7a4431fe32f
lifthus <lifthus531@gmail.com>
2023-10-28 15:44:03 +0900

    test

--- snofkid_test.go
+++ snofkid_test.go
@@ -4,6 +4,18 @@ import (
 	"testing"
 )
 
+func TestSnowflakeIDFrom(t *testing.T) {
+	sfid := SnowflakeIDFrom(123456789, 123, 10)
+	switch {
+	case RawTimestamp(sfid) != 123456789:
+		fallthrough
+	case MachineID(sfid) != 123:
+		fallthrough
+	case Sequence(sfid) != 10:
+		t.Errorf("SnowflakeIDFrom generated invalid Snowflake ID")
+	}
+}
+
 func TestRawTimestampGetter(t *testing.T) {
 	target := SnowflakeIDFrom(123456789, 1, 10)
 	if res := RawTimestamp(target); res != 123456789 {
@@ -24,21 +36,21 @@ func TestSequenceGetter(t *testing.T) {
 		t.Errorf("Sequence is expected to be %d but got %d", 123, res)
 	}
 }
-func TestMaxTimestamp(t *testing.T) {
+func TestMaxTimestampConst(t *testing.T) {
 	calcedMaxTs := int64(-1) ^ (int64(-1) << TimestampBits)
 	if MaxTimestamp != calcedMaxTs {
 		t.Errorf("MaxTimestamp is expected to be %d but got %d", calcedMaxTs, MaxTimestamp)
 	}
 }
 
-func TestMaxMachineID(t *testing.T) {
+func TestMaxMachineIDConst(t *testing.T) {
 	calcedMaxMID := -1 ^ (-1 << MachineIDBits)
 	if MaxMachineID != calcedMaxMID {
 		t.Errorf("MaxMachineID is expected to be %d but got %d", calcedMaxMID, MaxMachineID)
 	}
 }
 
-func TestMaxSequence(t *testing.T) {
+func TestMaxSequenceConst(t *testing.T) {
 	calcedMaxSeq := -1 ^ (-1 << SequenceBits)
 	if MaxSequence != calcedMaxSeq {
 		t.Errorf("MaxSequence is expected to be %d but got %d", calcedMaxSeq, MaxSequence)
